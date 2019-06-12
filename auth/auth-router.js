a95a8b5e8de09df95cb195446e1fada8a038da90
zchristy <zchristy44@gmail.com>
2019-06-12 12:58:45 -0500

    Completed Guided Project

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js')
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,8 +27,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // generate token
+        const token = generateToken(user)
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +41,19 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // standard claim = sub
+    username: user.username,
+    roles: ['student', 'TA']
+  }
+
+  const options = {
+    expiresIn: '1d',
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -3,12 +3,26 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, checkRole('admin'), (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      res.json({users, user: req.user});
     })
     .catch(err => res.send(err));
 });
+
+function checkRole(role) {
+  return function(req, res, next) {
+    if (req.user) {
+      if(req.user.roles && req.user.roles.includes(role)) {
+        next()
+      } else {
+        res.status(403).json({message: 'You dont have the right role'})
+      }
+    } else {
+      res.status(401).json({message: 'you can not pass'})
+    }
+  }
+}
 
 module.exports = router;
